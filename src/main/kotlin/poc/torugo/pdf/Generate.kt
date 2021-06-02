5d1b51c80c3731f66f2d6fd9918c9d0f765478a6
Breno Loyola <brenol@users.noreply.github.com>
2021-06-02 12:36:53 -0300

    test-commit

--- src/main/kotlin/poc/torugo/pdf/Generate.kt
+++ src/main/kotlin/poc/torugo/pdf/Generate.kt
@@ -1,16 +1,19 @@
 package poc.torugo.pdf
 
-import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
 // import com.openhtmltopdf.svgsupport.BatikSVGDrawer
+
+import com.openhtmltopdf.extend.FSSupplier
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
 import org.jboss.resteasy.annotations.jaxrs.FormParam
-import org.jboss.resteasy.annotations.providers.multipart.*
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm
+import org.jboss.resteasy.annotations.providers.multipart.PartType
 import java.io.*
-import javax.ws.rs.*
-import javax.ws.rs.core.*
-
-
-
-
+import javax.ws.rs.Consumes
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 
 class Body {
@@ -22,23 +25,23 @@ class Body {
 @Path("/convert/html")
 class Generate {
     val producer : String = "afyadigital.maagizo"
+    val fontSupplier = Supplier()
 
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.MULTIPART_FORM_DATA)
     fun hello(@MultipartForm body: Body ): Response {
+//        println(inputStreamAsString(fontIS))
         val file = body.file ?: return Response.status(400, "empty file").build()
-        val reader = file?.bufferedReader()
-
+        val reader = file.bufferedReader()
         val content = StringBuilder()
         reader.use { reader ->
-            var line = reader?.readLine()
+            var line = reader.readLine()
             while (line != null) {
                 content.append(line)
-                line = reader?.readLine()
+                line = reader.readLine()
             }
         }
-
         try {
             val stream = ByteArrayOutputStream()
             val builder = PdfRendererBuilder()
@@ -46,18 +49,29 @@ class Generate {
             builder.usePdfUaAccessbility(true)
             builder.usePdfAConformance(PdfRendererBuilder.PdfAConformance.PDFA_3_A)
             // builder.useSVGDrawer(BatikSVGDrawer())
+            builder.useFont(fontSupplier, "Arial")
             builder.withHtmlContent(content.toString(), null)
-            val fontURL = Generate::class.java.getResource("arial.ttf") ?: throw Exception("no font found")
-            builder.useFont(File(fontURL.toURI()), "Arial")
             builder.toStream(stream);
             builder.withProducer(this.producer)
-            builder.run();
+            builder.run()
             val response = Response.ok(stream.toByteArray())
             response.header("Content-Disposition", "attachment;filename=prescription.pdf")
             return response.build()
         } catch (e : Exception) {
             e.printStackTrace()
             return Response.serverError().build()
+        }
+    }
+
+}
+
+class Supplier : FSSupplier<InputStream?> {
+    override fun supply(): InputStream? {
+        return try {
+            this.javaClass.getResourceAsStream("/arial.ttf")
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+            null
         }
     }
 }
--- src/test/kotlin/poc/torugo/pdf/NativeGenerateIT.kt
+++
@@ -1,6 +0,0 @@
-package poc.torugo.pdf
-
-import io.quarkus.test.junit.NativeImageTest
-
-@NativeImageTest
-class NativeGenerateIT : GenerateTest()
