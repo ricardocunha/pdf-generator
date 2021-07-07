8f590e239c5617bffbaafdd98528dffd7dd8af56
Breno Loyola <brenol@users.noreply.github.com>
2021-07-07 17:22:13 -0300

    refac

--- src/main/kotlin/poc/torugo/pdf/Generate.kt
+++ src/main/kotlin/poc/torugo/pdf/Generate.kt
@@ -1,6 +1,6 @@
 package poc.torugo.pdf
 
-// import com.openhtmltopdf.svgsupport.BatikSVGDrawer
+import com.openhtmltopdf.svgsupport.BatikSVGDrawer
 
 import com.openhtmltopdf.extend.FSSupplier
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
@@ -31,7 +31,6 @@ class Generate {
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.MULTIPART_FORM_DATA)
     fun hello(@MultipartForm body: Body ): Response {
-//        println(inputStreamAsString(fontIS))
         val file = body.file ?: return Response.status(400, "empty file").build()
         val reader = file.bufferedReader()
         val content = StringBuilder()
@@ -43,18 +42,8 @@ class Generate {
             }
         }
         try {
-            val stream = ByteArrayOutputStream()
-            val builder = PdfRendererBuilder()
-            builder.useFastMode()
-            builder.usePdfUaAccessbility(true)
-            builder.usePdfAConformance(PdfRendererBuilder.PdfAConformance.PDFA_3_A)
-            // builder.useSVGDrawer(BatikSVGDrawer())
-            builder.useFont(fontSupplier, "Arial")
-            builder.withHtmlContent(content.toString(), null)
-            builder.toStream(stream);
-            builder.withProducer(this.producer)
-            builder.run()
-            val response = Response.ok(stream.toByteArray())
+            val baos = createBuilder(content)
+            val response = Response.ok(baos.toByteArray())
             response.header("Content-Disposition", "attachment;filename=prescription.pdf")
             return response.build()
         } catch (e : Exception) {
@@ -63,6 +52,20 @@ class Generate {
         }
     }
 
+    private fun createBuilder(content: StringBuilder): ByteArrayOutputStream {
+        val baos = ByteArrayOutputStream()
+        val builder = PdfRendererBuilder()
+        builder.useFastMode()
+        builder.usePdfUaAccessbility(true)
+        builder.usePdfAConformance(PdfRendererBuilder.PdfAConformance.PDFA_3_A)
+        builder.useSVGDrawer(BatikSVGDrawer())
+        builder.useFont(fontSupplier, "Arial")
+        builder.withHtmlContent(content.toString(), null)
+        builder.toStream(baos)
+        builder.withProducer(this.producer)
+        builder.run()
+        return baos
+    }
 }
 
 class Supplier : FSSupplier<InputStream?> {
